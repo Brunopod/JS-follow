e8119a09301fe82b93c22c8b0f9016842ac3aac7
Bruno Borak <borak13@gmail.com>
2022-01-27 13:12:00 +0000

    Add code to calculate two random numbers between 1 and 25

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,10 @@ document.addEventListener("DOMContentLoaded", function() {
 })
 
 function runGame() {
-
+    
+    //Creats two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
